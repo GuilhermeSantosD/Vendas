50516ecc46a8e078bf19b5339f2da2a5dad1e4d7
aspiranteDev <guilherme.bluesman@gmail.com>
2023-10-27 12:00:24 -0300

    Seller entitty class

--- src/application/program.java
+++ src/application/program.java
@@ -1,13 +1,21 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class program {
 
 	public static void main(String[] args) {
 
 		Department obj = new Department(1, "Books");
-		System.out.println(obj);
+		
+		Seller seller = new Seller (22, "guilherme", "guilherme@gmail.com", new Date(), 5000.0, obj  );
+		
+		
+		
+		System.out.println(seller);
 	}
 
 }
