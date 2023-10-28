66e1bdeb7edc2daadcf2b4fe7c2221f1c959609f
aspiranteDev <guilherme.bluesman@gmail.com>
2023-10-28 13:07:59 -0300

    Resusing instatiation

--- src/application/program.java
+++ src/application/program.java
@@ -11,6 +11,7 @@ public class program {
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
+		System.out.println("=== TEST 1: seller findById =====");
 		Seller seller = sellerDao.findById(3);
 		
 		System.out.println(seller);
