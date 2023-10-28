855f5de6a96120e54e142d8f3b728bd0958bb8ee
aspiranteDev <guilherme.bluesman@gmail.com>
2023-10-28 16:22:23 -0300

    findAll

--- src/application/program.java
+++ src/application/program.java
@@ -27,6 +27,12 @@ public class program {
 			
 		}
 	
+		System.out.println("\n=== TEST 3: seller findByAll=====");
+		 list = sellerDao.findAll();
+		for(Seller obj : list) {
+			System.out.println(obj);
+			
+		}
 	
 	}
 
