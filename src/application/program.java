4580f819ac8a3ddade99c78b9c84c8365092be2c
aspiranteDev <guilherme.bluesman@gmail.com>
2023-10-29 16:28:40 -0300

    Delete Seller

--- src/application/program.java
+++ src/application/program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -12,6 +13,8 @@ public class program {
 
 	public static void main(String[] args) {
 
+		Scanner sc = new Scanner (System.in);
+		
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
@@ -44,6 +47,17 @@ public class program {
 		seller.setName("Guilherme santos");
 		sellerDao.update(seller);
 		System.out.println("Update completed!");
-		}
+	
+		System.out.println("\n=== TEST 6: Seller delete=====");
+		System.out.println("Enter id for delete test");
+		int id = sc.nextInt();
+		sellerDao.deleteById(id);
+		System.out.println("Delete completed!");
+		
+		
+		
+		sc.close();
+	
+	}
 
 }
